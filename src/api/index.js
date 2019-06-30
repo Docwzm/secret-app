3330b1f6811fd17918c9dc17bb048ea7095f1523
wangdq <1030026236@qq.com>
2019-06-30 23:45:16 +0800

    comit

--- src/api/index.js
+++ src/api/index.js
@@ -1,9 +1,38 @@
 import request from '@/utils/request'
 
-export function getList(params) {
+const getSecret = (id) => {
     return request({
-        url: '/table/list',
-        method: 'get',
-        params
+        url: '/api/speech/'+id,
+        method: 'get'
     })
 }
+
+
+const saveSecret = (data) => {
+    return request({
+        url: '/api/speech',
+        method: 'post',
+        data
+    })
+}
+
+
+/**
+ * 上传语音
+ * @param {*} data 
+ */
+const uploadAudio = (file) => {
+    return request({
+      url: SERVICE_NAME + '/static/ueditor/1.4.3.3/php/controller.php?action=uploadvideo',
+      method: 'post',
+      headers: {'Content-Type':'multipart/form-data'},
+      data: file
+    })
+  }
+
+
+  export {
+    getSecret,
+    saveSecret,
+    uploadAudio
+  }
