d576e5dd923172ba0126a0b0888a82aac6e97f92
wangdq <1030026236@qq.com>
2019-07-02 22:03:04 +0800

    comit

--- src/utils/env.js
+++ src/utils/env.js
@@ -2,5 +2,5 @@ export function staticHost2ApiHost() {
     var mHost = window.location.hostname
     return {
         'localhost': 'http://shequgouvip.com:18003',
-    }[mHost] || ''
+    }[mHost] || 'http://shequgouvip.com:18003'
 }
