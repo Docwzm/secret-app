0ea9c7b726708d4b8e31ed16cfdc8c615b1e2b40
dongqiang.wang <1030026236@163.com>
2020-07-25 19:01:35 +0800

    域名调整

--- src/utils/env.js
+++ src/utils/env.js
@@ -3,6 +3,6 @@ export function staticHost2ApiHost() {
     return {
         'localhost': 'https://symy.powerionics.cn',
         'symy.powerionics.cn': 'https://symy.powerionics.cn',
-        'powerionics.vip': 'https://powerionics.vip',
-    }[mHost] || 'https://powerionics.vip'
+        'www.powerionics.vip': 'https://www.powerionics.vip',
+    }[mHost] || 'https://www.powerionics.vip'
 }
